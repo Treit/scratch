a25913dadf283ff39c9a768cc349e2013148a17b
Mike Treit <mtreit@ntdev.microsoft.com>
2024-11-12 13:25:20 -0800

    Demonstrate .NET 9 app using BinaryFormatter

---
+++ BinaryFormatterTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+var bf = new BinaryFormatter();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+var stream = new MemoryStream();
+bf.Serialize(stream, new TestRecord("Hello", 123));
+stream.Position = 0;
+var record = (TestRecord)bf.Deserialize(stream);
+Console.WriteLine(record);
+
+[Serializable]
+record TestRecord(string Name, int Value);
