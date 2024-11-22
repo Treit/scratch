88bbbcc3748063d6daa8bd5dd53fe07c65aa327d
Mike Treit <mtreit@ntdev.microsoft.com>
2024-11-21 17:51:37 -0800

    Try to reproduce an issue with the KTrie library.

---
+++ TrieIssue/Program.cs
@@ -0,0 +1,33 @@
+﻿using KTrie;
+
+var words = File.ReadAllLines("words.txt");
+
+var trie = new Trie();
+foreach (var word in words)
+{
+    trie.Add(word);
+}
+
+var tasks = new List<Task>();
+
+for (int i = 0; i < 1000; i++)
+{
+    tasks.Add(Task.Run(() =>
+    {
+        long count = 0;
+        while (true)
+        {
+            var matches = trie.StartsWith("a").Take(1000);
+            foreach (var match in matches.ToList())
+            {
+                count++;
+                if (count % 10_000_000 == 0)
+                {
+                    Console.Write(".");
+                }
+            }
+        }
+    }));
+}
+
+Task.WaitAll(tasks);
