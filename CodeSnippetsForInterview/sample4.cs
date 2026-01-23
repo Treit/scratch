d066b1a4266f6f502e2888ec462d340619e214e7
Mike Treit (from Dev Box) <mtreit@ntdev.microsoft.com>
2026-01-23 13:37:47 -0800

    Another sample

---
+++ CodeSnippetsForInterview/sample4.cs
@@ -0,0 +1,12 @@
+public class SomeClass
+{
+    public static HashSet<SomeEnum> Values => new HashSet<SomeEnum>()
+    {
+        SomeEnum.A,
+        SomeEnum.B,
+        SomeEnum.C,
+        SomeEnum.D,
+        SomeEnum.E,
+        SomeEnum.F
+    };
+}
